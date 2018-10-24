44d45e3b50e39188bc1c1a6834cc42190fb3318c
sxiao1 <sxiao1@stuy.edu>
2018-10-24 00:20:38 -0400

    working on index

--- SuperArray.java
+++ SuperArray.java
@@ -5,34 +5,18 @@ public class SuperArray{
 		data = new String[0];
 	}
 	public int size(){
-		int count = 0;
 		for(int x = 0; x < data.length; x++){
 			if (data[x] != null){
-				count ++;
+				size ++;
 			}
 		}
-		return count;
-	}
-	public boolean add(String newstr){
-		int firstNull = 10;
-		int other = 0;
-		for(int x = 0; x < data.length; x++){
-			if (data[x] == null){
-				other = x;
-			}
-			if(other < firstNull){
-				firstNull = other;
-			}
-		}
-		if (firstNull == 10){
-			return false;
-		}
-		else{
-			data[firstNull] = newstr;
-			return true;
-		}
+		int sz = size;
+		return sz;
 	}
 	public String get(int index){
+		if(data.length == 0){
+			return null;
+		}
 		String s = data[index];
 		return s;
 	}
@@ -41,4 +25,78 @@ public class SuperArray{
 		data[index] = element;
 		return s;
 	}
+	public String toString(){
+		String str= "[";
+		for(int x = 0; x < data.length; x ++){
+			if(data[x] != null){
+				str+= data[x] + ",";
+			}
+			if(x + 1 == data.length){
+				str = str.substring(0, str.length()-1);
+			}
+		}
+		str += "]";
+		return str;
+	}
+	public String toStringDebug(){
+		String str= "[";
+		for(int x = 0; x < data.length; x ++){
+			str+= data[x] + ",";
+			if(x+ 1 == data.length){
+				str = str.substring(0, str.length()- 1);
+			}
+		}
+		str += "]";
+		return str;
+	}
+	public boolean add(String newstr){
+		if (data.length == 0){
+			data = new String[10];
+		}
+		int other = 0;
+		for(int x = 0; x < data.length && other == 0; x++){
+			if (data[x] == null){
+				other = x;
+			}
+		}
+		if (other == 0){
+			return false;
+		}
+		else{
+			data[other] = newstr;
+			return true;
+		}
+	}
+	public void resize(){
+		String [] newdata = new String[data.length + 1];
+		for (int x = 0; x < data.length; x++){
+			newdata[x] = data[x];
+		}
+		newdata = data;
+	}
+	public boolean contains(String target){
+		for(int x = 0; x < data.length; x++){
+			if (data[x] == target){
+				return true;
+			}
+		}
+		return false;
+	}
+	/*public int indexOf(String target){
+		for(int x = 0; x < data.length; x++){
+			if (data[x] == target){
+				return x;
+			}
+		}
+	}
+	public int lastIndexOf(String target){
+		int index;
+		for(int x = 0; x < data.length; x++){
+			if (data[x]== target){
+				index = x;
+			}
+		}
+		return index;
+	} */
+
 }
