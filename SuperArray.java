91d3d0247ca521a09580bf387d0a10aa85a1c6af
sxiao1 <sxiao1@stuy.edu>
2018-10-25 21:28:38 -0400

    tested

--- SuperArray.java
+++ SuperArray.java
@@ -1,31 +1,28 @@
 public class SuperArray{
-	private String[] data = new String[10];
-	private int size;
+	private String[] data;
+	private int size = 0;
+	public SuperArray(){
+		data = new String[10];
+	}
+	public SuperArray(int x){
+		data = new String[x];
+	}
 	public void clear(){
 		data = new String[0];
+		size = 0;
 	}
 	public int size(){
-		if (data.length == 0){
-			size = 0;
-		}
-		int tempsz = 0;
-		for(int x = 0; x < data.length; x++){
-			if (data[x] != null){
-				tempsz ++;
-			}
-		}
-		tempsz = size;
 		return size;
 	}
 	public String get(int index){
 		if(data.length == 0|| index < 0 || index > data.length - 1){
-			return null;
+			return "null";
 		}
 		String s = data[index];
 		return s;
 	}
 	public String set(int index, String element){
-		if(data.length == 0){
+		if(index < 0 || index >= size){
 			return null;
 		}
 		String s = data[index];
@@ -33,12 +30,12 @@ public class SuperArray{
 		return s;
 	}
 	public String toString(){
-		String str= "[";
+		String str = "[";
 		for(int x = 0; x < data.length; x ++){
 			if(data[x] != null){
 				str+= data[x] + ",";
 			}
-			if(x + 1 == data.length){
+			if(size != 0 && x + 1 == data.length){
 				str = str.substring(0, str.length()-1);
 			}
 		}
@@ -57,54 +54,35 @@ public class SuperArray{
 		return str;
 	}
 	public boolean add(String newstr){
-		if (data.length == 0){
-			data = new String[10];
+		if (size == data.length){
+			resize();
 		}
-		int other = -1;
-		for(int x = 0; x < data.length && other == -1; x++){
-			if (data[x] == null){
-				other = x;
-			}
-		}
-		if (other == -1){
-			return false;
-		}
-		else{
-			data[other] = newstr;
-			return true;
-		}
+		data[size] = newstr;
+		size ++;
+		return (data[size - 1] == newstr);
 	}
 	public void resize(){
-		String [] newdata = new String[data.length * 2];
+		SuperArray newary = new SuperArray(data.length * 2);
 		for (int x = 0; x < data.length; x++){
-			newdata[x] = data[x];
+			newary.data[x] = data[x];
 		}
-		newdata = data;
+		data = newary.data;
 	}
 	public boolean isEmpty(){
-		String element = "";
-		for(int x = 0; x < data.length; x++){
-			if (data[x] != null){
-				element = data[x];
-			}
-		}
-		if (element.equals("")){
-			return true;
-		}
-		else{
-			return false;
-		}
+		return (size == 0);
 	}
 	public void add(int index, String element){
 		if(index >= data.length|| index < 0){
 			System.out.println("error");
 		}
-		if(data.length == size){
-			resize();
-		}
-		for(int x = index + 1; x < data.length; x++){
+		for(int x = size - index + 1; x > index; x--){
+			if(data.length == size){
+				resize();
+			}
 			data[x] = data[x-1];
 		}
+		size ++;
+		set(index, element);
 	}
 	public boolean contains(String target){
 		for(int x = 0; x < data.length; x++){
@@ -116,20 +94,20 @@ public class SuperArray{
 	}
 	public int indexOf(String target){
 		int index = data.length;
-		int tempindex = 0;
+		int tempindex = data.length;
 		for(int x = 0; x < data.length; x++){
 			if (data[x] == target){
 				tempindex = x;
 			}
 			if(tempindex < index){
-				tempindex = index;
+				index = tempindex;
 			}
 		}
 		return index;
 	}
 	public int lastIndexOf(String target){
 		int index = -1;
-		int tempindex = 0; 
+		int tempindex = 0;
 		for(int x = 0; x < data.length; x++){
 			if (data[x]== target){
 				tempindex = x;
@@ -139,7 +117,7 @@ public class SuperArray{
 			}
 		}
 		return index;
-	} 
+	}
 	public String remove(int index){
 		String element = data[index];
 		if (index >= data.length || index < 0){
